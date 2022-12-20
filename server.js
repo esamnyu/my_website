a64dd89435a1f9d8342d6d4f5e7712d7f04a682f
esamnyu <105534679+esamnyu@users.noreply.github.com>
2022-12-19 23:33:36 -0500

    Updated Server.js file

--- server.js
+++ server.js
@@ -1,25 +1,48 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
+
+const uri = "mongodb+srv://<user>:<pass>@cluster0.mongodb.net/test?retryWrites=true&w=majority";
+
+MongoClient.connect(uri, function(err, client) {
+  if (err) {
+    console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+  }
+  console.log('Connected...');
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 const app = express();
 
-const url = 'mongodb://localhost:27017';
-const dbName = 'mydatabase';
-
-MongoClient.connect(url, { useNewUrlParser: true }, (error, client) => {
-  if (error) {
-    console.log('Error occurred while connecting to MongoDB:', error);
-  } else {
-    console.log('Connected to MongoDB successfully!');
-    const db = client.db(dbName);
-  }
+app.get('/', function (req, res) {
+  res.send('Hello World');
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
+
+// Home page route
+app.get('/', function (req, res) {
+  res.send('Welcome to my website');
+});
+
+// About page route
+app.get('/about', function (req, res) {
+  res.send('About me');
+});
+
+// Contact page route
+app.get('/contact', function (req, res) {
+  res.send('Contact me');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+app.post('/contact', function (req, res) {
+    // Handle contact form submission
+  });
+
